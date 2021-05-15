492566e062d307168c2aed8f76e22e713ece15e0
DebashishPaul <debashish333paul@gmail.com>
2021-05-15 22:13:45 +0600

    ecommerce done

--- store/admin.py
+++ store/admin.py
@@ -1,5 +1,15 @@
 from django.contrib import admin
 from .models import Product, Order
+
+#admin panel modify
+admin.site.site_header = "E-com store"
+admin.site.site_title = "admin panel"
+admin.site.index_title = "Table"
+
+#show all product details
+class ProductAll(admin.ModelAdmin):
+    list_display = ('name','product_desc','real_price')
+    search_fields = ('name','real_price')
 # Register your models here.
-admin.site.register(Product)
+admin.site.register(Product, ProductAll)
 admin.site.register(Order)
